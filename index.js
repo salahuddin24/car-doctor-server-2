3797c0f215ffc555538c62e98b5cbc43ee22a6f6
SALAHUDDIN <salahuddin152541@gmail.com>
2024-06-25 21:31:44 +0600

    added booking service

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const port = process.env.PORT || 5000;
 // middle ware 
 app.use(cors());
 app.use(express.json());
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Security-Policy", "default-src 'none'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com;");
+//   next();
+// });
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.2vx7h3g.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +32,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
 
 
     app.get('/services', async(req, res) =>{
@@ -35,6 +40,37 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+
+    // load specific service 
+    app.get('/services/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection : {title: 1, price: 1, service_id: 1, img: 1}
+      }
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    })
+
+    // bookings
+    app.post('/bookings', async(req, res) =>{
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
