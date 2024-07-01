f09ea214fe03e16fbf99ba820e078ac19c49fd90
SALAHUDDIN <salahuddin152541@gmail.com>
2024-07-01 23:45:54 +0600

    added jwt with middle ware

--- _jwt_steps_.js
+++ _jwt_steps_.js
@@ -24,3 +24,8 @@
  * 3. client side axios setting
   in axios set withCredentials: true
  */
+
+  /***
+   * 1. to send cookies from the client make sure you added withCreadentials true for the api call using axios
+   * 2. use cookie parser as middleware
+   */
--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const port = process.env.PORT || 5000;
 
 // middle ware 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin:['http://localhost:5173', 'http://localhost:5174'],
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
+
 
 
 
@@ -30,6 +31,35 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middle ware create by i 
+const logger = async(req, res, next) =>{
+  console.log('called:', req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async(req, res, next) =>{
+  const token = req.cookies?.token;
+  console.log('value of token in middlewate', token);
+
+  if(!token){
+    return res.status(401).send({message: 'not authorized'});
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    //error 
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'unauthorized'});
+    }
+    //if token is valid then it would be decoded
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next();
+  })
+
+  
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -37,23 +67,23 @@ async function run() {
 
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
-  
+
     // auth related api 
-    app.post('/jwt', async(req, res)=>{
+    app.post('/jwt', logger,  async(req, res)=>{
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+     
       res
       .cookie('token', token, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'none'
+        secure: false
       })
       .send({success : true});
     })
 
     // services related api 
-    app.get('/services', async(req, res) =>{
+    app.get('/services', logger,  async(req, res) =>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -73,9 +103,13 @@ async function run() {
 
     // bookings
 
-    app.get('/bookings', async(req, res) =>{
-      // console.log(req.query.email);
-      console.log('tok tok token', req.cookies.token);
+    app.get('/bookings', verifyToken,  logger, async(req, res) =>{
+      console.log(req.query.email);
+      // console.log('tok tok token', req.cookies.token);
+      console.log('user in the valid token', req.user);
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
@@ -111,13 +145,6 @@ async function run() {
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
     })
-
-
-
-
-
-
-
 
 
 
